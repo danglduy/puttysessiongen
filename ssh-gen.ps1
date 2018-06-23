4b8d846df08dcd0da9e9a4e09f44b1058f0ce80b
zldang <38782010+zldang@users.noreply.github.com>
2018-06-23 23:43:25 +0700

    Initial files upload

---
+++ ssh-gen.ps1
@@ -0,0 +1,41 @@
+﻿$sessionname = Read-Host 'Your session name'
+$IP = Read-Host 'IP'
+$IP.replace(' ','') *>&1 | out-null
+
+
+### Edit your variable here
+# The path to putty session registry file folder.
+# This is also the location of default.reg.
+$regpath = $(pwd).Path
+# The path to putty.exe file
+$puttypath = $(pwd).Path+'\putty.exe'
+# The path to for your result session files.
+$puttysshpath = $(pwd).Path
+# Private key file name
+$privatekeyname = 'private.ppk'
+# The path to private key (.ppk) of the session
+$privatekeypath = $(pwd).Path+'\'+$privatekeyname
+### END
+
+
+# The path to putty session default template file.
+$defaultreg = $regpath+'default.reg'
+# The path to putty session customized reg file.
+$sessionreg = $regpath+$sessionname+'.reg'
+
+### Do not edit below
+$privatekeypath -replace "\\", "\\"
+
+(Get-Content $defaultreg)  | Foreach-Object { $_ -replace '\$IP',$IP`
+                                                 -replace '\$sessionname',$sessionname`
+                                                 -replace '\$privatekeypath',$privatekeypath
+                                            } > $sessionreg
+
+Reg Import $sessionreg *>&1 | out-null
+Remove-Item -Path $sessionreg 
+$puttysshcontent = 'start '+$puttypath+' -ssh -load "'+$sessionname+'"'
+$puttysshfile = $sessionname + '.cmd'
+$puttysshcontent | Out-File -encoding ASCII $puttysshpath$puttysshfile 
+
+
+
